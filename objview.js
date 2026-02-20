5de904edfcc4b67dcd6d71b2d397af4910c11749
Karanvir Singh <165871054+that-kvs@users.noreply.github.com>
2026-02-20 13:28:34 +0530

    asdasf

---
+++ objview.js
@@ -0,0 +1,62 @@
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+import { OBJLoader } from "jsm/loaders/OBJLoader.js";
+import getLayer from './getLayer.js';
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+function init(geometry) {
+  const material = new THREE.MeshMatcapMaterial({
+    matcap: new THREE.TextureLoader().load('./obj1/texdefrag_1.obj_optimized_texture_0.png'),
+    // transparent: true,
+    // opacity: 0.5
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const sunlight = new THREE.DirectionalLight(0xffffff);
+  sunlight.position.y = 2;
+  scene.add(sunlight);
+
+  const filllight = new THREE.DirectionalLight(0x88ccff);
+  filllight.position.x = 1;
+  filllight.position.y = -2;
+  scene.add(filllight);
+
+  // Sprites BG
+  const gradientBackground = getLayer({
+    hue: 0.6,
+    numSprites: 8,
+    opacity: 0.2,
+    radius: 10,
+    size: 24,
+    z: -10.5,
+  });
+  scene.add(gradientBackground);
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+  animate();
+}
+
+const loader = new OBJLoader();
+loader.load("./obj1/1.obj", (obj) => init(obj.children[0].geometry));
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
